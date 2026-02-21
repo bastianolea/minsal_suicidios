6c5aa36ce16109a47aeee6f43569f17bb60d9b42
bastianolea <bastianolea@gmail.com>
2026-02-21 00:21:30 -0300

    primera versión

--- funciones.R
+++ funciones.R
@@ -19,6 +19,7 @@ tema_sin_ejes <- \() {
 }
 
 grafico_circulos_encima <- function(datos) {
+  # browser()
   datos |> 
     mutate(horizontal = case_match(genero,
                                    "femenino" ~ 1,
@@ -33,6 +34,14 @@ grafico_circulos_encima <- function(datos) {
           fill = genero, color = genero),
       linewidth = .3, alpha = 0.9
     ) |> blend("multiply") +
+    geom_text(
+      aes(x = horizontal, y = vertical, label = number(valor)),
+      size = 5, fontface = "bold", color = color$fondo, alpha = 0.8
+    ) +
+    geom_text(
+      aes(x = horizontal, y = vertical-(max(horizontal)*0.08), label = genero),
+      size = 3, fontface = "plain", color = color$fondo, alpha = 0.8
+    ) +
     coord_fixed() +
     escala_genero() +
     tema_sin_fondo() +
@@ -112,4 +121,56 @@ grafico_lineas <- function(datos) {
     theme(axis.text.x = element_text(face = "bold", color = color$texto, size = 11, vjust = 0),
           axis.title.y = element_text(vjust = 1, margin = margin(r = 4))) +
     theme(axis.line = element_line(color = color$detalle, lineend = "round", linewidth = 1))
+}
+
+limpiar_causas <- function(datos) {
+  datos |> 
+    mutate(glosa = str_replace(glosa_categoria, "autoinfligido intencionalmente por, y exposición (al|a)", "por")) |> 
+    mutate(glosa = str_remove(glosa, "autoinfligida intencionalmente ")) |> 
+    mutate(glosa = str_remove_all(glosa, "Lesión por|Envenenamiento por "),
+           glosa = str_remove_all(glosa, ", y (los|las) no especificad(a|o)s|, no clasificadas en otra parte"),
+           glosa = str_remove_all(glosa, "antiepilépticas,|, antiparkinsonianas"),
+           glosa = str_remove_all(glosa, "^otr(a|o)s"),
+           glosa = str_replace(glosa, "otras armas", "armas"),
+           glosa = str_replace(glosa, "medios especificados", "medios"),
+           glosa = str_squish(glosa) |> str_trim()) |> 
+    mutate(glosa = str_to_sentence(glosa))
+}
+
+grafico_causas <- function(datos_diag_conteo, datos_diag_wide) {
+  datos_diag_conteo |> 
+    ggplot() +
+    aes(x = n, y = glosa, color = genero) +
+    # segmentos uniendo los puntos
+    geom_segment(data = datos_diag_wide,
+                 aes(x = min, xend = max, 
+                     y = glosa, yend = glosa,
+                     color = mayor), 
+                 inherit.aes = F, size = 1.3, alpha = .5) +
+    # textos
+    geom_text(data = datos_diag_wide,
+              aes(x = max, y = glosa, color = mayor, label = number(max)),
+              nudge_x = 3, hjust = 0,
+              size = 3, fontface = "bold") |> 
+    ggblend::copy_over(color = color$texto, alpha = 0.4) +
+    geom_text(data = datos_diag_wide,
+              aes(x = min, color = menor, y = glosa, label = number(min)),
+              nudge_x = -3, hjust = 1,
+              size = 3, fontface = "bold") |> 
+    ggblend::copy_over(color = color$texto, alpha = 0.4) +
+    # punto transparente
+    geom_point(size = 8, alpha = 0.3) +
+    # punto normal
+    geom_point(size = 4) +
+    scale_y_discrete(labels = function(x) str_wrap(x, width = 36)) +
+    scale_x_continuous(labels = number, 
+                       breaks = seq(0, 50, by = 10)) +
+    theme(axis.text.y = element_text(color = color$texto,
+                                     lineheight = 1),
+          axis.title.y = element_text(color = "#9F8B97"),
+          axis.title.x = element_text(vjust = 0, margin = margin(t = 4)),
+          panel.grid.major.x = element_line(linetype = "solid")) +
+    theme(legend.position = "none") +
+    labs(y = "Lesiones autoinflingidas intencionalmente",
+         x = "Cantidad de víctimas letales por género")
 }
--- graficos.R
+++ graficos.R
@@ -156,27 +156,29 @@ datos_año |>
 # causas ----
 datos_diag <- arrow::read_parquet("datos/minsal_suicidios_diagnostico.parquet")
 
-datos_diag <- datos_diag |> 
-  mutate(glosa_categoria = str_replace(glosa_categoria, 
-                                       "autoinfligido intencionalmente por, y exposición (al|a)",
-                                       "por"),
-         glosa_categoria = str_remove(glosa_categoria, 
-                                      "autoinfligida intencionalmente ")
-  )
+# datos_diag |> 
+#   count(glosa_categoria, sort = T) |> 
+#   print(n=Inf)
 
+# procesar_
+
+
+
+datos_diag <- datos_diag |> 
+  limpiar_causas()
 
 datos_diag |> 
-  count(glosa_categoria, sort = T)
+  count(glosa, sort = T)
 
 datos_diag |> 
   filter(condicion_egreso == "consumado") |>
-  count(genero, glosa_categoria, sort = T) |> 
+  count(genero, glosa, sort = T) |> 
   pivot_wider(names_from = genero, values_from = n, values_fill = 0) |> 
   print(n=Inf)
 
 datos_diag |> 
   filter(condicion_egreso == "consumado") |>
-  count(genero, glosa_categoria, sort = T) |> 
+  count(genero, glosa, sort = T) |> 
   group_by(genero) |> 
   mutate(p = n / sum(n)) |> 
   select(-n) |> 
@@ -185,25 +187,18 @@ datos_diag |>
   filter(masculino + femenino > 0.05) |> 
   print(n=Inf)
 
-
-
-datos_diag_2 <- datos_diag |> 
+datos_diag_conteo <- datos_diag |> 
   filter(condicion_egreso == "consumado") |>
-  count(genero, glosa_categoria, sort = T) |> 
+  count(genero, glosa, sort = T) |> 
   group_by(genero) |> 
   mutate(p = n / sum(n)) |> 
-  group_by(glosa_categoria) |> 
+  group_by(glosa) |> 
   mutate(total = sum(n)) |> 
   ungroup() |> 
-  filter(total > 5) |> 
-  mutate(glosa = str_remove_all(glosa_categoria, "Lesión por|Envenenamiento por "),
-         glosa = str_remove_all(glosa, ", y (los|las) no especificad(a|o)s|, no clasificadas en otra parte"),
-         glosa = str_remove_all(glosa, "antiepilépticas,|, antiparkinsonianas"),
-         glosa = str_remove_all(glosa, "^otr(a|o)s"),
-         glosa = str_replace(glosa, "otras armas", "armas"),
-         glosa = str_squish(glosa))
+  filter(total > 4) |> 
+  mutate(glosa = forcats::fct_reorder(glosa, n))
 
-datos_diag_w <- datos_diag_2 |>
+datos_diag_wide <- datos_diag_conteo |>
   select(-total, -p) |> 
   pivot_wider(names_from = genero, values_from = n, 
               values_fill = 0) |> 
@@ -212,27 +207,6 @@ datos_diag_w <- datos_diag_2 |>
   mutate(max = max(femenino, masculino),
          min = min(femenino, masculino))
 
-datos_diag_2 |> 
-  mutate(glosa = forcats::fct_reorder(glosa, n)) |> 
-  ggplot() +
-  aes(x = n, y = glosa, 
-      color = genero) +
-  geom_segment(data = datos_diag_w,
-               aes(x = min, xend = max, 
-                   y = glosa, yend = glosa,
-                   color = mayor), 
-               inherit.aes = F, size = 0.4) +
-  geom_text(data = datos_diag_w,
-            aes(x = max, y = glosa, label = number(max)),
-            nudge_x = 2, hjust = 0,
-            size = 3, fontface = "bold", color = color$texto) +
-  geom_text(data = datos_diag_w,
-            aes(x = min, y = glosa, label = number(min)),
-            nudge_x = -2, hjust = 1,
-            size = 3, fontface = "bold", color = color$texto) +
-  geom_point(size = 4) +
-  scale_y_discrete(labels = function(x) str_wrap(x, width = 30)) +
-  theme(axis.text.y = element_text(lineheight = 0.7)) +
-  theme(legend.position = "none") +
-  labs(y = "Lesiones autoinflingidas intencionalmente",
-       x = "Cantidad de víctimas letales por género")
+
+datos_diag_conteo |> 
+  grafico_causas(datos_diag_wide)
